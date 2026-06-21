bab79d518949f22872a6aa100353c23362d8953e
Aditya-lab442 <chauhanaditya1109@gmail.com>
2026-06-21 13:06:31 +0530

    Authentication

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,21 @@
+import userModel from '../models/userModel.js'
+
+//add item to user cart
+
+const addToCart = async (req, res) => {
+    
+}
+
+//remove items from user cart
+
+const removeFromCart = async (req, res) => {
+
+}
+
+// fetch user cart data
+
+const getCart = async (req, res) => {
+
+}
+
+export { addToCart, removeFromCart, getCart }
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,19 +5,19 @@ import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, discription, image }) => {
     // const [itemCount, setItemCount] = useState(0)
-    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
+    const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext)
 
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
-                <img src={image} alt="" className="food-item-image" />
+                <img src={url+"/images/"+image} alt="" className="food-item-image" />
                 {!cartItems[id]
                     ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
                     : <div className='food-item-counter'>
                         <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
                         <p>{cartItems[id]}</p>
                         <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
-                        
+
                     </div>
                 }
             </div>
