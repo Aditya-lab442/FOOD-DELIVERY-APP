4b87ad74018dd1ba96640b2f1b307108c3027622
Aditya-lab442 <chauhanaditya1109@gmail.com>
2026-06-26 12:40:19 +0530

    Admin Panel Order Page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -75,13 +75,37 @@ const verifyOrder = async (req, res) => {
 
 const userOrders = async (req, res) => {
     try {
-        const orders = await orderModel.find({userId: req.body.userId});
-        res.json({success:true,data:orders})
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data: orders })
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
-        
+        res.json({ success: false, message: "Error" })
+
     }
 }
 
-export { placeOrder, verifyOrder, userOrders }
+//Listing Order For Admin Panel
+
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({})
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+// API for updating order status
+
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus }
